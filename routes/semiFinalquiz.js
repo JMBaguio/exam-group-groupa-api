7905562f544d4f50b533ddec0c48d34f73c31155
unknown <YY324@ccs.local>
2025-03-27 06:10:26 -0700

    feat: add GET /exams endpoint

--- routes/semiFinalquiz.js
+++ routes/semiFinalquiz.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const router = express.Router;
+
+let exams = [
+    {id:1, subject: 'Science', date: '2025-4-1'}
+];
+
+router.get('/exams', (req, res) => {
+    res.json(exams);
+});
