9be784e1c2b3e9c98ca5e8d873c876f82913f5bb
unknown <YY324@ccs.local>
2025-03-27 06:04:33 -0700

    feat: add PUT /exams endpoint

--- routes/semiFinalquiz.js
+++ routes/semiFinalquiz.js
@@ -1,2 +1,25 @@
 const express = require('express')
 const router = express.Router;
+
+router.put("/exams/:id" , (req,res) => {
+    const examId = parseInt(req.params.id);
+    const updateExam = req.body;
+
+    let found = false;
+    exams = exams.map((exam) => {
+        if (exam.id == examId) {
+            found = true;
+            return {... exam, ...updateExam};
+        }
+        return exam;
+    })
+
+    if(found) {
+        res.json({message:"Exam updated successfully"});
+    }
+    else {
+        res.status(404).json({error: "Exam not found"});
+    }
+});
+
+module.exports = router;
